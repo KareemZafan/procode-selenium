99866aaf705d5c70e676f1a71972fec07c799f4c
KareemZafan <kareemm.zafan@gmail.com>
2024-06-30 18:28:39 +0300

    Docker Session

--- src/main/java/browser_actions/DriverFactory.java
+++ src/main/java/browser_actions/DriverFactory.java
@@ -16,7 +16,7 @@ import java.net.URL;
 import java.util.List;
 
 public class DriverFactory {
-    private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+    private static final ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
 
     public WebDriver getDriver() {
         return drivers.get();
@@ -49,19 +49,53 @@ public class DriverFactory {
     }
 
     public void setRemoteDriver(String browserName) {
-        List<String> arguments = List.of("--disable-notifications", "--disable-popup-blocking", "--incognito", "--headless"); //"--headless"
+        List<String> arguments = List.of("--disable-notifications", "--disable-popup-blocking", "--incognito");
         //todo configure options for multiple different browsers
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments(arguments);
-        options.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, "dismiss");
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setAcceptInsecureCerts(true);
-        caps.setBrowserName(browserName);
-        options.merge(caps);
-        try {
-            drivers.set(new RemoteWebDriver(new URL("http://localhost:4444"), options));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                ChromeOptions options = new ChromeOptions();
+                //options.addArguments(arguments);
+                options.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, "dismiss");
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setAcceptInsecureCerts(true);
+                caps.setBrowserName(browserName);
+                options.merge(caps);
+                try {
+                    drivers.set(new RemoteWebDriver(new URL("http://localhost:4444  "), options));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "firefox":
+                FirefoxOptions options1 = new FirefoxOptions();
+                options1.addArguments(arguments);
+                options1.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, "dismiss");
+                DesiredCapabilities caps1 = new DesiredCapabilities();
+                caps1.setAcceptInsecureCerts(true);
+                caps1.setBrowserName(browserName);
+                options1.merge(caps1);
+                try {
+                    drivers.set(new RemoteWebDriver(new URL("http://localhost:4444"), options1));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "edge":
+                EdgeOptions options2 = new EdgeOptions();
+                options2.addArguments(arguments);
+                options2.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, "dismiss");
+                DesiredCapabilities caps2 = new DesiredCapabilities();
+                caps2.setAcceptInsecureCerts(true);
+                caps2.setBrowserName(browserName);
+                options2.merge(caps2);
+                try {
+                    drivers.set(new RemoteWebDriver(new URL("http://localhost:4444"), options2));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported Browser!");
         }
     }
 
@@ -70,9 +104,7 @@ public class DriverFactory {
     }
 
     public enum BrowserType {
-        CHROME,
-        FIREFOX,
-        EDGE
+        CHROME, FIREFOX, EDGE
 
         //todo add more drivers such a safari, opera ...
     }
